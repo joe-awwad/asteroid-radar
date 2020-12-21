a43c2977649cca91bfa2fd8dd0cd0a1c9c7d688b
Joe Awwad <joe.awwad@getray.com>
2020-12-21 11:23:54 +0200

    Binds progress bar to asteroid list

--- starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.udacity.asteroidradar
 
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
@@ -38,4 +39,9 @@ fun bindTextViewToKmUnit(textView: TextView, number: Double) {
 fun bindTextViewToDisplayVelocity(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.km_s_unit_format), number)
+}
+
+@BindingAdapter("goneIfNotNull")
+fun goneIfNotNull(view: View, it: Any?) {
+    view.visibility = if (it != null) View.GONE else View.VISIBLE
 }
